6f83832852cf2b41287bc1fe388939f672acec07
HuihanCui <114428325+HuihanCui@users.noreply.github.com>
2023-04-02 05:13:49 +0800

    Delete comp0004_2 directory

--- comp0004_2/src/main/java/comp0004/model/element/thing/Thing.java
+++
@@ -1,8 +0,0 @@
-package comp0004.model.element.thing;
-
-
-public interface Thing {
-    void editContent(String newContent);
-
-    String getContent();
-}
--- comp0004_2/src/main/webapp/header.jsp
+++
@@ -1,3 +0,0 @@
-<div>
-    <h1>List webapp</h1>
-</div>
